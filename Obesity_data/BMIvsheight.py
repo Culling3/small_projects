dedbf1433d764b6142bacb53262a8809c77fd814
Aktan Gelbal <128340670+Culling3@users.noreply.github.com>
2024-02-18 20:08:13 +0300

    Update BMIvsheight.py

--- Obesity_data/BMIvsheight.py
+++ Obesity_data/BMIvsheight.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
 read = pd.read_csv("obesity_data.csv")
 
 bmi = read[["BMI"]]
 height = read["Height"]
-
+bmim = bmi.mean()
+bmim1 = bmim.iloc[0]
+heightm = height.mean()
+print(heightm)
 
 linr = LinearRegression()
 linr.fit(bmi, height)
@@ -20,5 +24,10 @@ plt.xlabel('BMI')
 plt.ylabel('Height')
 plt.title('Linear Regression: BMI vs Height')
 
+correlation_matrix = np.corrcoef(bmi.values.flatten(), height.values.flatten())
+correlation_coefficient = correlation_matrix[0, 1]
+print(f"Correlation coeeficient: {correlation_coefficient}")
+print(f"R^2 score: {linr.score(bmi, height)}")
+print(f"Average bmi: {bmim1:.2f}")
 plt.legend()
 plt.show()
